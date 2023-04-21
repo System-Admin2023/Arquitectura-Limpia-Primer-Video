0a8001a9cd6d7de934cb018398d895a4c856dd86
System-Admin2023 <131399214+System-Admin2023@users.noreply.github.com>
2023-04-20 22:50:14 -0500

    Add files via upload

---
+++ Arquitectura Limpia/NorthWind/NorthWind.UseCasesDTOs/CreateOrder/CreateOrderParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCasesDTOs.CreateOrder
+{
+    public class CreateOrderParams
+    {
+        public string CustomerId { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public string ShipPotalCote { get; set; }
+        public List<CreateOrderDetailParams> OrderDetails { get; set; }
+
+
+    }
+}
